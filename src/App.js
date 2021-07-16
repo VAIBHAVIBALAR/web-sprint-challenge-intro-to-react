075ec971bce4f28b0100282b9689f08be2119b60
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-07-16 12:04:54 -0700

    initialtestcommit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Character from './components/Character'
+import styled  from 'styled-components';
+
+const StyleDiv = styled.div`
+color: white;
+padding: 2% 5%;
+margin: 8% 15%;
+background-image: url("https://wallpapercave.com/wp/boHuSO8.png");
+font-family: "Times New Roman", Times, serif;
+`
+
+
+
+
+
+
+
+
 
 const App = () => {
+  const[character , setCharacter] = useState([])
+
+  const takeCharacterData = () =>{
+    axios.get('https://swapi.dev/api/people')
+    .then((res) =>{
+      console.log(res.data)
+      setCharacter(res.data)
+    })
+    .then(res =>{
+      console.log(res.data)
+      console.log(character)
+    })
+    .catch(err => console.log(err))
+
+  }
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+useEffect(() =>{
+  takeCharacterData()
+}, [])
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyleDiv className="App">
+      <h1 className="Header">STAR-WARS CHARACTER</h1>
+     <Character  character = {character}/>
+    </StyleDiv>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
-// Write your Character component here
+import React from  'react';
+import styled  from 'styled-components';
+
+const StyleList = styled.div`
+padding: 2% 2%;
+background-image: url("https://cdn.wallpapersafari.com/30/92/YHzVDv.jpg");
+font-family: 'Lato';
+`
+function Character(props) {
+    return  <StyleList>
+    {props.character.map(index => 
+    <ul>{index.name}
+    <li>{index.gender}</li>
+    <li>{index.birth_year}</li>
+    <li>{index.mass}</li>
+    <li>{index.height}</li>
+    </ul>)}
+    </StyleList>
+}
+export default Character;
