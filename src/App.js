8dee7713d40d17e7fc63fa8450f91d8159e9408b
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-07-16 15:28:51 -0700

    StretchgoalcommitforStyledcomponents

--- src/App.js
+++ src/App.js
@@ -5,32 +5,37 @@ import Character from './components/Character'
 import styled  from 'styled-components';
 import themeObj from './themes'
 
+//by using styled components
 const StyleDiv = styled.div`
-color: white;
-padding: 0;
-margin: 0;
-height: 100%;
-background-repeat: no-repeat;
-background-position-y: 100%;
-background-size: cover;
-font-family: Arial Black;
-`
-
+  color: white;
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position-y: 100%;
+  background-size: cover;
+  font-family: Arial Black;
+  @media ${props => props.theme.breakpointMobile}{
+    width: initial;
+    padding: 2% 5% 4% 0%;
+  }
+  `
+//using state hooks
 const App = () => {
-  const[character , setCharacter] = useState([])
+  const[character , setCharacter] = useState([]);
 
+  //function to fetch data from API
   const takeCharacterData = () =>{
-    axios.get('https://swapi.dev/api/people')
-    .then((res) =>{
-      console.log(res.data)
-      setCharacter(res.data)
+      axios.get('https://swapi.dev/api/people')
+       .then((res) =>{
+         console.log(res.data)
+         setCharacter(res.data)
     })
-    .then(res =>{
-      console.log(res.data)
-      console.log(character)
+      .then(res =>{
+        console.log(res.data)
+        console.log(character)
     })
     .catch(err => console.log(err))
-
   }
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -38,14 +43,19 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  //for side effect
 useEffect(() =>{
   takeCharacterData()
 }, [])
+ 
+
   return (
-    <StyleDiv className="App">
-      <h1 className="Header">STAR-WARS CHARACTER</h1>
-     <Character  character = {character}/>
-    </StyleDiv>
+    //using styled component in place of div
+     <StyleDiv className="App">
+         <h1 className="Header">STAR-WARS CHARACTER</h1>
+         <Character  character = {character}/>
+     </StyleDiv>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,19 +1,42 @@
 import React from  'react';
-import styled  from 'styled-components';
+import styled, { keyframes }  from 'styled-components';
 import '../App.css';
 
+//Stretch goals styled components
+const kf = keyframes`
+100% {
+  opacity : 1;
+  transform: scale(1) rotateZ(0);
+    }
+`
+//styled components for custom component
 const StyleList = styled.div`
 font-size: 1.5em;
 padding: 2% 30%;
 font-family: 'Lato';
+opacity: 0;
+transform: scale(2) ;
+animation: ${kf} 0.5s ease-in-out forwards;
+@media ${props => props.theme.breakpointMobile}{
+    width: initial;
+    padding: 2% 2%;
+    margin: 2% 6%;
+  }
 `
+
+const StyleInList = styled.span`
+opacity: 0;
+transform: scale(2) rotateZ(360deg);
+animation: ${kf} 0.5s ease-in-out forwards;
+`
+//Custom components
 function Character(props) {
     return  <StyleList>
-    {props.character.map(index => 
-    <div className="List">
-        <span >{index.name}</span>
-        <span className="Span">{index.birth_year}</span>
-        </div> )}
-    </StyleList>
+                 {props.character.map(index => 
+                    <div className="List">
+                         <StyleInList >{index.name}</StyleInList>
+                         <StyleInList className="Span">{index.birth_year}</StyleInList>
+                    </div> )}
+             </StyleList>
 }
 export default Character;
